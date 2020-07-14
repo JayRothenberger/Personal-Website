8ee8c6b33a4a90da808b80b7d3328ab08327b582
JayRothenberger <JayRothenberger@users.noreply.github.com>
2020-07-14 11:17:26 -0400

    changelog 7.14.1.1
    secret key is now generated for improved secrecy
    added margins to scoreboard rows

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import random
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b*os+j%sfz=qt9+o^8xm+8=ga4@94i@ug23=&=*a#hig-p5ypl'
+# Is this stupid?  Probably.
+SECRET_KEY = "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)])
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
