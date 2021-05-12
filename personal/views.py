4c6ea74b21abd8d795e2fb158f7a30fb36eeece4
JayRoth-sch <jr2fh@virginia.edu>
2021-05-12 09:40:43 -0400

    it seems we also have to call the function

--- personal/views.py
+++ personal/views.py
@@ -410,7 +410,7 @@ def test(request):
     query = request.GET.get('query')
     response = HttpResponse()
     try:
-        response.content = json.dumps({'error': None, 'return': main()})
+        response.content = json.dumps({'error': None, 'return': main([query])})
     except Exception as e:
         response.content = json.dumps({'error':str(e), 'return': None})
 
