dfedc86efe940e007c34555eabbbfb95e3863eff
jroth <jay.c.rothenberger@gmail.com>
2026-01-15 15:25:00 -0600

    adding links url

--- personal/urls.py
+++ personal/urls.py
@@ -4,14 +4,11 @@ from . import views
 app_name = 'personal'
 urlpatterns = [
     path('', views.index_view, name='index'),
-    path('test/', views.test, name='test'),
-    path('code/', views.code, name='code'),
     path('ponder/', views.test, name='ponder'),
     path('blog/', views.blog, name='blog'),
-    path('ide/', views.ide, name='ide'),
     path('run/', views.run, name='run'),
     path('rendezvous/', views.rendezvous, name='rendezvous'),
     path('cv/', views.cv, name='cv'),
     path('resume/', views.resume, name='resume'),
-    path('publications/', views.publications, name='publications'),
+    path('links/', views.publications, name='links'),
 ]
